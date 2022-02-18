a1650360a405baab2a5262c6865b15eb2d74a474
NidushaAmarasinghe <95688464+NidushaAmarasinghe@users.noreply.github.com>
2022-02-18 07:36:46 -0800

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -86,13 +86,15 @@ async def about(client, message):
 async def start(client: Client, message: Message):
     try:
         await message.reply_text(
-            text=script.START_MSG.format(message.from_user.mention),
+            text=script.GRPSTART_MSG.format(message.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("HELP", callback_data="help_data"),
-                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                        InlineKeyboardButton(
+                            "PM",
+                            url="https://t.me/STMImage_Editor_Bot",
+                        )
                     ],
                     [
                         InlineKeyboardButton(
