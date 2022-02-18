08a555c2cf56b7dfc7c09f9d27a1845266995757
NidushaAmarasinghe <95688464+NidushaAmarasinghe@users.noreply.github.com>
2022-02-18 07:46:10 -0800

    Update script.py

--- script.py
+++ script.py
@@ -18,22 +18,7 @@ For more click help....</b>"""
 
 © @STM_Developers"""
     
-        GRPSTART_MSG = """ <b>Hi 
 
-Use Me In PM [Private]
-For more click help....</b>"""
-    HELP_MSG = """Hi {}, Follow these Steps..
-
-<code>🌀 Send me any Image to Edit..</code>
-
-<code>🌀 Select the Corresponding mode that you need</code>
-
-<code>🌀 Your Edited Image will be Uploaded </code>
-
-
-
-
-© @STM_Developers"""
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
